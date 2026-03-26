73ec5fb77663e88c97bcc5c40fb5ba39f12f4b75
Franco <franco.rlam@estudiante.uc.cl>
2026-03-26 17:12:56 -0300

    Añadir pasos

--- Juego.py
+++ Juego.py
@@ -2,4 +2,6 @@ def instrucciones ():
     pass
 
 def pasos_para_jugar ():
-    pass
+    lista_opciones = ["piedra", "papel", "tijeras", "lagartija", "spock"]
+    mano_jugador = (input("Elige una de las opciones a continuación: Piedra, Papel, Tijeras, Lagartija o Spock: ")).lower()
+    mano_npc = lista_opciones[r.randint(0,4)]
