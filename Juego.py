9e48b97b8ee65d73dd1ebf47919fac4466bc9d37
NicoMedina123 <you@example.com>
2026-03-26 17:29:00 -0300

    papel

--- Juego.py
+++ Juego.py
@@ -29,7 +29,7 @@ def pasos_para_jugar ():
     mano_npc = lista_opciones[r.randint(0,4)]
     if mano_jugador == mano_npc:
         print("Empatamos nub.")
-    elif (mano_jugador == "piedra" and (mano_npc == "tijeras" or mano_npc == "lagartija")) or (mano_jugador == "paper" and (mano_npc == "piedra" or mano_npc == "spock")) or (mano_jugador == "tijeras" and (mano_npc == "paper" or mano_npc == "lagartija")) or (mano_jugador == "lagartija" and (mano_npc == "paper" or mano_npc == "spock")) or (mano_jugador == "spock" and (mano_npc == "piedra" or mano_npc == "tijeras")):
+    elif (mano_jugador == "piedra" and (mano_npc == "tijeras" or mano_npc == "lagartija")) or (mano_jugador == "pape" and (mano_npc == "piedra" or mano_npc == "spock")) or (mano_jugador == "tijeras" and (mano_npc == "pape" or mano_npc == "lagartija")) or (mano_jugador == "lagartija" and (mano_npc == "pape" or mano_npc == "spock")) or (mano_jugador == "spock" and (mano_npc == "piedra" or mano_npc == "tijeras")):
         print("Me ganaste crack.")
-    elif (mano_jugador == "piedra" and (mano_npc == "paper" or mano_npc == "spock")) or (mano_jugador == "paper" and (mano_npc == "tijeras" or mano_npc == "lagartija")) or (mano_jugador == "tijeras" and (mano_npc == "piedra" or mano_npc == "spock")) or (mano_jugador == "lagartija" and (mano_npc == "piedra" or mano_npc == "tijeras")) or (mano_jugador == "spock" and (mano_npc == "paper" or mano_npc == "lagartija")):
+    elif (mano_jugador == "piedra" and (mano_npc == "pape" or mano_npc == "spock")) or (mano_jugador == "pape" and (mano_npc == "tijeras" or mano_npc == "lagartija")) or (mano_jugador == "tijeras" and (mano_npc == "piedra" or mano_npc == "spock")) or (mano_jugador == "lagartija" and (mano_npc == "piedra" or mano_npc == "tijeras")) or (mano_jugador == "spock" and (mano_npc == "pape" or mano_npc == "lagartija")):
         print("Perdiste nub.")
