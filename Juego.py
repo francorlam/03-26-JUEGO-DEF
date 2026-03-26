b9b033f9d1edc14409aad8b11c85bb2bd3afd71f
NicoMedina123 <you@example.com>
2026-03-26 17:17:11 -0300

    mensaje1

--- Juego.py
+++ Juego.py
@@ -1,5 +1,25 @@
 def instrucciones ():
-    pass
+    mensaje = """
+    Bienvenido a Piedra, Papel, Tijera, Lagartija, Spock
+
+    Reglas del juego:
+    - Piedra aplasta Tijera y Lagartija
+    - Papel cubre Piedra y refuta a Spock
+    - Tijera corta Papel y decapita Lagartija
+    - Lagartija come Papel y envenena a Spock
+    - Spock vaporiza Piedra y rompe Tijera
+
+    Cada opción gana contra 2 y pierde contra 2.
+
+    Elige una opción:
+    1. Piedra
+    2. Papel
+    3. Tijera
+    4. Lagartija
+    5. Spock
+    """
+
+    print(mensaje)
 
 def pasos_para_jugar ():
     lista_opciones = ["piedra", "papel", "tijeras", "lagartija", "spock"]
